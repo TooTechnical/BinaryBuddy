6d2ca7bfc4019e8deb37d5a4f51ebded0c7a37ff
Dillon Malone <dillonsmalone.94@gmail.com>
2025-02-12 21:31:04 +0000

    Am I here? I am losing my mind

--- script.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Check the directory structure of the BinaryBuddy project
-for root, dirs, files in os.walk("/workspace/BinaryBuddy"):
-    print(f"Directory: {root}")
-    for file in files:
-        print(f"  File: {file}")
